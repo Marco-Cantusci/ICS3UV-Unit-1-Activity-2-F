70bc2ff8304dba5359ede8517612b600482a785a
default <marco.cantusci@stu.ocsb.ca>
2025-09-26 04:09:16 +0000

    Completed fix

--- main.go
+++ main.go
@@ -14,19 +14,19 @@ func main() {
 	fmt.Println("Year Born       Year Graduated            Result")
 
 	// addition
-	fmt.Println("2009         +       2027                 = " + fmt.Sprint(2009 + 2027))
+	fmt.Println("2009         +       2027                 = " + fmt.Sprint(2009+2027))
 
 	// subtraction
-	fmt.Println("2009         -       2027                 = " + fmt.Sprint(2009 - 2027))
+	fmt.Println("2009         -       2027                 = " + fmt.Sprint(2009-2027))
 
 	// multiplication
-	fmt.Println("2009         *       2027                 = " + fmt.Sprint(2009 * 2027))
+	fmt.Println("2009         *       2027                 = " + fmt.Sprint(2009*2027))
 
 	// division
-	fmt.Println("2009         /       2027                 = " + fmt.Sprint(2009 / 2027))
+	fmt.Println("2009         /       2027                 = " + fmt.Sprint(2009/2027))
 
 	// modulo
-	fmt.Println("2009         %       2027                 = " + fmt.Sprint(2009 % 2027))
+	fmt.Println("2009         %       2027                 = " + fmt.Sprint(2009%2027))
 
 	fmt.Println("\nDone.")
 
