d05bb5989126ef6a1c12710c39762a621c8a7d7c
default <marco.cantusci@stu.ocsb.ca>
2025-09-26 03:51:58 +0000

    Completed activity

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
 	fmt.Println("2009         *       2027                 ="    + fmt.Sprint(2009 * 2027))
 
 	// division
-	fmt.Println("2009         /       2027                 ="    + fmt.Sprint(2009.0 / 2027.0))
+	fmt.Println("2009         /       2027                 ="    + fmt.Sprint(2009 / 2027))
 
 	// modulo
 	fmt.Println("2009         %       2027                 ="    + fmt.Sprint(2009 % 2027))
