3aaeeb7a0cba37191d1228a581f8de9ad33af079
default <marco.cantusci@stu.ocsb.ca>
2025-09-26 03:47:00 +0000

    Completed activity

---
+++ main.go
@@ -0,0 +1,33 @@
+/**
+ * @author Marco Cantusci
+ * @verion 1.0.0
+ * @date 2025-09-25
+ * @fileoverview Math problem
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Year Born       Year Graduated            Result")
+
+	// addition
+	fmt.Println("2009         +       2027                 ="    + fmt.Sprint(2009 + 2027))
+
+	// subtraction
+	fmt.Println("2009         -       2027                 ="    + fmt.Sprint(2009 - 2027))
+
+	// multiplication
+	fmt.Println("2009         *       2027                 ="    + fmt.Sprint(2009 * 2027))
+
+	// division
+	fmt.Println("2009         /       2027                 ="    + fmt.Sprint(2009.0 / 2027.0))
+
+	// modulo
+	fmt.Println("2009         %       2027                 ="    + fmt.Sprint(2009 % 2027))
+
+	fmt.Println("\nDone.")
+
+}
---
+++ main.ts
@@ -0,0 +1,28 @@
+/**
+ * @author Marco Cantusci
+ * @verion 1.0.0
+ * @date 2025-09-25
+ * @fileoverview Math problem
+ */
+
+
+console.log("Year Born       Year Graduated            Result")
+
+// addition
+console.log("2009         +       2027                 =" + (2009 + 2027))
+
+
+// subtraction
+console.log("2009         -       2027                 =" + (2009 - 2027))
+
+
+// multiplication
+console.log("2009         *       2027                 =" + (2009 * 2027))
+
+// division
+console.log("2009         /       2027                 =" + (2009.0 / 2027.0))
+
+// modulo
+console.log("2009         %       2027                 =" + (2009 % 2027))
+
+console.log("\nDone.")
